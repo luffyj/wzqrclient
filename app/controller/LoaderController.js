bcfe63ed14e85ddb284e9f68858f846bce95e578
luffyj <luffy.ja@gmail.com>
2014-08-05 01:26:27 +0800

    add id

--- app/controller/LoaderController.js
+++ app/controller/LoaderController.js
@@ -1,10 +1,14 @@
 Ext.define('wzqr.controller.LoaderController', {
     extend: 'wzqr.controller.BaseController',    
     requires:[
+        'Ext.MessageBox',
         'Ext.Ajax'
     ],    
     init:function(x){
         debug('LoaderController init',x);
         this.checkAuth();
+        //<debug>
+        Ext.Msg.alert('警告','这里只是UI测试界面，请路过');
+        //</debug>
     }
 });
